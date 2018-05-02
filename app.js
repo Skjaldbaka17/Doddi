b64091617b834be94538b8240153ceff2c068298
Skjaldbaka17 <Skjaldbaka17@gmail.com>
2018-05-02 23:24:55 +0000

    first Commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const router = require('./router');
+const readdirAsync = util.promisify(fs.readdir);
+const readFileAsync = util.promisify(fs.readFile);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', router);
+
+//Ef síðan er ekki til.
+function notFoundHandler(req, res, next) { // eslint-disable-line
+    const title = 'Fannst ekki';
+    const message = 'Sorry, this page is currently under construction.';
+    res.status(404).render('error', {message });
+  }
+  
+  /*Ef upp kemur villa. Hinsvegar væri hægt að gera betur með því að 
+    gefa upp meiri upplýsingar með status-kóðanum (default hér 500)*/
+  function errorHandler(err, req, res, next) { // eslint-disable-line
+    console.error(err);
+    const title = 'Villa kom upp';
+    const message = '';
+    res.status(500).render('error', { message });
+  }
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+
+
+const hostname = 'localhost';
+const port = 4000;
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
+
---
+++ router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+async function home(req, res){
+    res.render('home');
+}
+
+async function product(req, res){
+    res.render('product');
+}
+
+function catchErrors(fn) {
+    return (req, res, next) => fn(req, res, next).catch(next);
+}
+
+router.get('/', catchErrors(home));
+router.get('/:product', catchErrors(product));
+
+module.exports = router;
