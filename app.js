95275bdd879195bd33606f585c4b951dcf5ba66a
Skjaldbaka17 <Skjaldbaka17@gmail.com>
2018-05-03 16:15:59 +0000

    Stýlun

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const readdirAsync = util.promisify(fs.readdir);
 const readFileAsync = util.promisify(fs.readFile);
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/img')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
